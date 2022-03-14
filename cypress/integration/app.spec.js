3d62b8ff78025214da598c6bc9e8c490b912decc
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-14 22:20:58 +0400

    write first test to check app

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Given that I am any user', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('root_url'))
+  })
+
+  context('When I access homepage', () => {
+    it('It should contain calendar', () => {
+      cy.contains('Calendar app')
+    })
+  })
+})
