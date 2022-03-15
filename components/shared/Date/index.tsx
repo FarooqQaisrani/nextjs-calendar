07daa87d3c8a1ececfb2f1f6404ae57732065782
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-15 20:37:30 +0400

    update tests

--- components/shared/Date/index.tsx
+++ components/shared/Date/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Date: React.FC<Props> = (props) => {
   return (
-    <td className={`h-10 w-10 ${props.className}`}>
+    <td className={`h-10 w-10 ${props.className}`} data-testid="date">
       <p>{props.date}</p>
     </td>
   )
--- cypress/integration/calendar.spec.js
+++ cypress/integration/calendar.spec.js
@@ -10,10 +10,10 @@ describe('Given that I am any user', () => {
       cy.get('[data-testid="calendar"]').should('exist')
     })
     it('It should have Month', () => {
-      cy.get('[data-testid="month"]').should('not.be.empty')
+      cy.get('[data-testid="month"]').contains('March')
     })
     it('It should have Dates', () => {
-      cy.get('[data-testid="date"]').should('not.be.empty')
+      cy.get('[data-testid="date"]').should('have.length.above', 0)
     })
   })
 })
