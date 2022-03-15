ba01b2c4dd671ad5067c23ebb9ad487cd4d3c62c
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-15 20:54:46 +0400

    add test for calendar basic navigation

---
+++ cypress/integration/calendar-navigation.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Given that I am any user', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('root_url'))
+  })
+
+  context('When I access homepage', () => {
+    it('It should have Month Navigation', () => {
+      cy.get('[data-testid="btn-next-month"]').should('exist')
+      cy.get('[data-testid="btn-next-month"]').should('exist')
+    })
+  })
+})
