6ea2bbb34f2e92593052f292b18d039bad92587c
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-16 17:53:19 +0400

    add test for unavailable dates

--- cypress/integration/suite-date-range-select.spec.js
+++ cypress/integration/suite-date-range-select.spec.js
@@ -11,3 +11,22 @@ describe('Given that I am any user', () => {
     })
   })
 })
+
+describe('Given that I am any user', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('root_url') + 'suite/1')
+    cy.intercept('api/unavailable-dates', {
+      fixture: 'unavailable-dates.json',
+    }).as('unavailableDatesApi')
+  })
+
+  context('When I access Suite detail page', () => {
+    it('It should filter out unavailable dates', () => {
+      cy.contains('Suite Date Selector')
+      cy.wait('@unavailableDatesApi')
+      for (let i = 21; i < 24; i++) {
+        cy.contains(i).should('have.class', 'unavailable')
+      }
+    })
+  })
+})
---
+++ pages/api/unavailable-dates.ts
@@ -0,0 +1,55 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Dates = {
+  startDate: string
+  endDate: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<Dates>>
+) {
+  res.status(200).json([
+    {
+      startDate: '2021-09-27',
+      endDate: '2022-03-29',
+    },
+    {
+      startDate: '2022-03-30',
+      endDate: '2022-04-12',
+    },
+    {
+      startDate: '2022-04-13',
+      endDate: '2022-04-25',
+    },
+    {
+      startDate: '2022-04-26',
+      endDate: '2022-05-31',
+    },
+    {
+      startDate: '2022-07-02',
+      endDate: '2022-07-16',
+    },
+    {
+      startDate: '2021-09-27',
+      endDate: '2022-03-29',
+    },
+    {
+      startDate: '2022-10-12',
+      endDate: '2022-10-18',
+    },
+    {
+      startDate: '2022-10-22',
+      endDate: '2022-10-28',
+    },
+    {
+      startDate: '2022-11-18',
+      endDate: '2022-11-25',
+    },
+    {
+      startDate: '2022-11-28',
+      endDate: '2022-12-04',
+    },
+  ])
+}
