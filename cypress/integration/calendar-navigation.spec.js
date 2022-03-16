a49e22bed8504101ad0cdaacfbb2482682ccbf60
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-16 14:23:25 +0400

    update navigation test

--- cypress/integration/calendar-navigation.spec.js
+++ cypress/integration/calendar-navigation.spec.js
@@ -9,6 +9,8 @@ describe('Given that I am any user', () => {
     it('It should have Month Navigation', () => {
       cy.get('[data-testid="btn-next-month"]').should('exist')
       cy.get('[data-testid="btn-prev-month"]').should('exist')
+      cy.get('[data-testid="btn-next-month"]').click()
+      cy.contains('April')
     })
   })
 })
