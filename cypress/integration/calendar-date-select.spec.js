b1d5cd22086315245d1d16896ed1caa8520b4fd7
Farooq Qaisrani <farooqqaisrani@outlook.com>
2022-03-16 16:34:10 +0400

    add test for stay end date

--- cypress/integration/calendar-date-select.spec.js
+++ cypress/integration/calendar-date-select.spec.js
@@ -5,10 +5,18 @@ describe('Given that I am any user', () => {
     cy.visit(Cypress.env('root_url'))
   })
 
-  context('When I access homepage', () => {
-    it('It should have Month Navigation', () => {
+  context('When I access Date Range Selector', () => {
+    it('I can select FROM date', () => {
       cy.contains('24').click()
       cy.contains('24').should('have.class', 'selected')
+    })
+
+    it('I can select END date', () => {
+      cy.contains('24').click()
+      cy.contains('30').click()
+
+      cy.contains('24').should('have.class', 'selected')
+      cy.contains('30').should('have.class', 'selected')
     })
   })
 })
